f3ca944e3321e2ee5c07a6eff48d9ad98c33d471
Alejandra Luna <alunam2@est.ups.edu.ec>
2025-11-12 13:54:15 -0500

    añadir archivo test_analizador

---
+++ test/test_analizador.py
@@ -0,0 +1,29 @@
+import unittest
+from src.procesador import Analizador
+
+class TestAnalizador(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.analizador = Analizador("datos/sri_ventas_2024.csv")
+
+    def test_ventas_totales_como_diccionario(self):
+        resumen = self.analizador.ventas_totales_por_provincia()
+        self.assertIsInstance(resumen, dict)
+
+    def test_ventas_totales_todas_las_provincias(self):
+        resumen = self.analizador.ventas_totales_por_provincia()
+        total_provincias = len(resumen)
+        self.assertEqual(total_provincias, 24)
+
+    def test_ventas_totales_mayores_5k(self):
+        resumen = self.analizador.ventas_totales_por_provincia()
+        self.assertTrue(all(float(v) > 5000 for v in resumen.values()))
+
+    def test_ventas_por_provincia_inexistente(self):
+        with self.assertRaises(KeyError):
+            self.analizador.ventas_por_provincia("Narnia")
+
+    def test_ventas_por_provincia_existente(self):
+        resultado = self.analizador.ventas_por_provincia("pichincha")
+        self.assertGreater(resultado, 0)
